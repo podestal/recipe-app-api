7bbd8744ee57f995a2dff112ded14b99bfd623c6
podestal <luisrodriguezpodesta@gmail.com>
2024-08-25 11:21:32 -0400

    adding new test

---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+"""
+Sample Test
+"""
+
+from django.test import SimpleTestCase
+from . import calc
+
+class CalcTests(SimpleTestCase):
+    """Test calc module"""
+
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
