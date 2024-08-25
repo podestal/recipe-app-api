2d33b06f8e592b2f92edbb29aa889d9ca60b5062
podestal <luisrodriguezpodesta@gmail.com>
2024-08-25 19:28:39 -0400

    db setup in docker compose

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,3 +6,7 @@ Calculator functions
 def add(x, y):
     """Add x and y and return result."""
     return x + y
+
+def substract(x, y):
+    """Substracts x and y and return result."""
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -14,3 +14,9 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
+
+    def test_substract_numbers(self):
+        """Test substract numbers."""
+        res = calc.substract(10, 15)
+
+        self.assertEqual(res, 5)
