e2c0a4fae782417f2ce81b02a7a4a18363d202f1
podestal <luisrodriguezpodesta@gmail.com>
2024-08-25 11:26:12 -0400

    spaces fixed

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,9 +1,8 @@
 """
-Calculator Function
+Calculator functions
 """
 
-def add(x, y):
 
-    """Add x and y and return the result"""
-    
+def add(x, y):
+    """Add x and y and return result."""
     return x + y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,15 +1,16 @@
 """
-Sample Test
+Sample tests
 """
-
 from django.test import SimpleTestCase
-from . import calc
+
+from app import calc
+
 
 class CalcTests(SimpleTestCase):
-    
-    """Test calc module"""
+    """Test the calc module."""
 
     def test_add_numbers(self):
-        """Test adding numbers together"""
+        """Test adding numbers together."""
         res = calc.add(5, 6)
+
         self.assertEqual(res, 11)
